2086b0501a43ad8e14498d187e83837e8e3bb278
3illBaby <thrillanon@outlook.com>
2024-02-02 16:31:45 +0100

    commit: initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,215 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { staggerContainer, slideIn } from '@/utils/motion';
+import Image from 'next/image';
+import { Check, CornerDownRight } from 'lucide-react';
+import { TitleText, TypingText } from '@/components/CustomTexts';
+import MiningCards from '@/components/MiningCards';
+import Footer from '@/components/Footer';
+
+const Landing = () => {
+  return (
+    <motion.div
+      variants={staggerContainer(0.1, 0.1)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className=" text-white text-4xl lg:text-6xl relative "
+    >
+      <div className="flex flex-row lg:min-h-screen padding-x bg-banner bg-cover bg-no-repeat ">
+        <div className="flex flex-col gap-3">
+          <TitleText
+            title="The First Digital Currency You Can Mine On Your Phone"
+            textStyles="text-4xl w-[350px] sm:w-[600px] lg:w-[1300px] lg:text-6xl tracking-wide capitalize font-medium font-lexend pt-5 mt-[35px]"
+          />
+          <motion.p
+            variants={slideIn('up', 'tween', 0.5, 0.75)}
+            initial="hidden"
+            whileInView="show"
+            className="text-[18px] sm:text-[20px] tracking-wide mt-[50px] leading-7 font-bold"
+          >
+            Start mining Pi cryptocurrency today with our free, energy-light{' '}
+            <br />
+            mobile app!
+          </motion.p>
+
+          <button className=" flex flex-row items-center gap-5 border-2 border-yellow-400 rounded-md px-8 py-2 lg:py-5 lg:mt-3 font-bold text-yellow-400 hover:scale-110 active:scale-100 group hover:text-white transition-all duration-200 max-w-[300px] text-[18px] ">
+            <TypingText
+              title="validate your wallet"
+              textStyles="text-yellow-400 hover:text-white transition-all duration-200"
+            />
+            <Check
+              color="yellow"
+              className="w-5 h-5 group-hover:translate-x-1 transition-all"
+            />
+          </button>
+        </div>
+
+        <Image
+          src="/pi_video-6.webp"
+          alt="phone"
+          width={500}
+          height={500}
+          className="hidden lg:flex absolute left-[1370px]  -top-[90px] "
+        />
+      </div>
+
+      {/* YouTube video embed */}
+      <div className="video-container padding-x lg:hidden md:flex sm:flex mt-5">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/MsOaC61cR3U"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {/**Mining is Hard section */}
+      <div className=" bg-purple-600 w-full">
+        <div className="flex flex-col gap-3 padding-y items-center justify-center text-[20px] sm:text-[32px] font-semibold">
+          <motion.p
+            variants={slideIn('up', 'spring', 0.5, 0.75)}
+            initial="hidden"
+            whileInView="show"
+            className="tracking-wide"
+          >
+            Mining crypto is hard
+          </motion.p>
+          <motion.p
+            variants={slideIn('up', 'spring', 0.6, 0.75)}
+            initial="hidden"
+            whileInView="show"
+            className="tracking-wide"
+          >
+            Investing crypto is risky
+          </motion.p>
+
+          <motion.p
+            variants={slideIn('up', 'spring', 0.7, 0.75)}
+            initial="hidden"
+            whileInView="show"
+            className="tracking-wide"
+          >
+            Too many of us are left out of <br /> the cryptocurrency revolution
+          </motion.p>
+        </div>
+      </div>
+      <div className=" bg-purple-400 w-full h-3"></div>
+      <div className=" bg-purple-300 w-full h-3"></div>
+      <div className=" bg-purple-200 w-full h-3"></div>
+
+      {/**Mining Easy Section */}
+      <section className="padding-x lg:px-[450px] bg-white items-center ">
+        <div className="flex flex-col lg:flex-row-reverse items-center gap-5">
+          <Image
+            src="/MiningEasy.png"
+            alt="mining_easy"
+            width={400}
+            height={400}
+          />
+          <div className="flex flex-col gap-5">
+            <h3 className=" text-4xl lg:text-5xl font-lexend font-bold  text-purple-600 tracking-wide">
+              Pi makes crypto <br className="hidden lg:block" /> mining easy.
+            </h3>
+            <p className=" text-gray-700 text-[18px] lg:text-[20px] font-semibold leading-7">
+              Breakthrough tech allows you to mine Pi on your phone without{' '}
+              <br className="hidden md:block lg:hidden" />
+              draining your battery.
+            </p>
+            <a
+              href="https://minepi.com/white-paper/"
+              target="_blank"
+              className=" flex flex-row items-center gap-5 border-2 bg-purple-600 shadow-sm rounded-md px-8 py-2 lg:py-5 lg:mt-3 font-bold hover:scale-110 active:scale-100 group hover:text-white transition-all duration-200 max-w-[350px] text-[18px] "
+            >
+              <TypingText
+                title="Learn the tech behind pi"
+                textStyles="text-white hover:text-white transition-all duration-200 capitalize"
+              />
+              <CornerDownRight
+                color="white"
+                className="w-5 h-5 group-hover:translate-x-1 transition-all"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col px-[50px] md:px-[200px] lg:px-0 lg:flex-row gap-5 mt-[100px] pb-5">
+          <MiningCards
+            index={1 as number}
+            icon="/Spinner.png"
+            title="Decentralized"
+            description="Secure, Immutable & Interoperable"
+          />
+          <MiningCards
+            index={2 as number}
+            icon="/iPhone.png"
+            title="Mobile First"
+            description="Not battery intensive"
+          />
+          <MiningCards
+            index={3 as number}
+            icon="/Internet.png"
+            title="User & Planet Friendly"
+            description="Easy to use, scale & eco-friendly "
+          />
+        </div>
+      </section>
+
+      {/**Download section */}
+      <section className="downloadGradient flex flex-col pb-5 ">
+        <div className="flex flex-col lg:flex-row lg:px-[300px] gap-5 lg:gap-[100px] ">
+          <Image
+            src="/download.webp"
+            alt="phone"
+            width={200}
+            height={200}
+            className="self-center"
+          />
+
+          <div className="flex flex-col gap-5 mt-[50px] padding-x md:px-[100px]">
+            <h3 className=" font-lexend text-[24px] sm:text-[32px] lg:text-[50px]  text-yellow-300 ">
+              Download the mobile app <br /> to start mining today! Join now.
+            </h3>
+
+            <p className=" text-white text-[16px] sm:text-[18px] lg:text-[20px] font-semibold leading-7 self-center  tracking-wide ">
+              Keep your money! Mining Pi is free. <br /> All you need is an
+              invitation from an existing trusted member on the network. <br />{' '}
+              If you have an invitation you can download the mobile app below.{' '}
+            </p>
+
+            <a
+              href="https://play.google.com/store/apps/details?id=com.blockchainvault&pli=1"
+              target="_blank"
+            >
+              <Image
+                src="/google_play.webp"
+                alt="google_play"
+                width={300}
+                height={89}
+              />
+            </a>
+
+            <a
+              href="https://apps.apple.com/us/app/pi-network/id1445472541"
+              target="_blank"
+            >
+              <Image
+                src="/apple_store.webp"
+                alt="google_play"
+                width={300}
+                height={89}
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      {/**Footer */}
+      <Footer />
+    </motion.div>
+  );
+};
+export default Landing;
---
+++ components/Navbar.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { navVariants, staggerContainer } from '@/utils/motion';
+import { navLinks, socialLinks } from '@/constants';
+import { ChevronDown, Search, Menu } from 'lucide-react';
+
+const Navbar = () => {
+  const [navbarColor, setNavbarColor] = useState('bg-transparent');
+  const navbarHeight = 80;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+
+      const navbarColor =
+        offset > navbarHeight ? 'bg-purple-800' : 'bg-transparent';
+      setNavbarColor(navbarColor);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [navbarHeight]);
+  return (
+    <motion.section
+      variants={staggerContainer(0.1, 0.1)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`sticky top-0 z-50 shadow ${navbarColor} transition-all duration-400 `}
+    >
+      <motion.nav
+        variants={navVariants}
+        whileInView="show"
+        className="padding-x py-4 flex flex-row  items-center  "
+      >
+        <Image
+          src="/Pi-Network.webp"
+          alt="Pi-Network"
+          width={300}
+          height={300}
+          className=" w-[100px] md:w-[100px] lg:w-[300px]"
+        />
+        <div className="hidden sm:hidden md:hidden lg:flex flex-row gap-3 text-gray-200 font-lexend ml-[500px]">
+          {navLinks.map((link) => (
+            <React.Fragment key={link.name}>
+              <div className="flex flex-row gap-2 items-center group hover:scale-105 active:scale-100 transition-all duration-200 hover:text-white">
+                <a href={link.link} className="relative overflow-hidden ">
+                  {link.name}
+                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-200 group-hover:w-full"></span>
+                </a>
+                <ChevronDown className="w-4 h-4" />
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+        <div className="hidden lg:flex flex-row gap-5 text-gray-200 font-lexend  ml-[100px] perspective">
+          {socialLinks.map((link) => (
+            <a
+              key={link.name}
+              href={link.link}
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-row items-center group hover:scale-105 flip-effect flip-on-hover active:scale-100 transition-all duration-200 hover:text-white w-5 h-5"
+            >
+              {link.icon}
+            </a>
+          ))}
+
+          <div className="ml-5 flip-effect group hover:scale-105 flip-on-hover transition-all active:scale-100">
+            <Search className="w-5 h-5" />
+          </div>
+        </div>
+
+        {/**Mobile nav */}
+        <div className="flex flex-row ml-[250px] md:ml-[500px] items-center  lg:hidden  gap-7 ">
+          <Search color="white" className="w-7 h-7" />
+          <Menu color="white" className="w-7 h-7" />
+        </div>
+      </motion.nav>
+    </motion.section>
+  );
+};
+export default Navbar;
